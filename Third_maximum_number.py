3411eef1038879a156862e0e31782ede9f072d79
Parth Jain <parthjain7721@gmail.com>
2026-06-05 09:40:26 +0530

    new codes

---
+++ Third_maximum_number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        first = float('-inf')
+        second = float('-inf')
+        third = float('-inf')
+        
+        for num in nums:
+            if num == first or num == second or num == third:
+                continue
+                
+            if num > first:
+                third = second
+                second = first
+                first = num
+            elif num > second:
+                third = second
+                second = num
+            elif num > third:
+                third = num
+                
+        return first if third == float('-inf') else third
